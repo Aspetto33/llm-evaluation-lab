7e6f7e2f71053e086b32ba1ec76e68f5160c40d8
Aspetto343 <1760906445@qq.com>
2026-07-15 23:16:23 +0800

    load testcases from json file and run testcases

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import time
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import json
+import datetime
+
 
 load_dotenv()
 
@@ -12,6 +15,7 @@ client = OpenAI(
     base_url=os.getenv("DEEPSEEK_BASE_URL")
 )
 
+#link deepseek llm
 def call_llm(prompt):
     try:
         start_time = time.perf_counter()
@@ -33,7 +37,8 @@ def call_llm(prompt):
                   "latency": round(latency, 3),
                   "prompt_tokens": response.usage.prompt_tokens,
                   "completion_tokens": response.usage.completion_tokens,
-                  "total_tokens": response.usage.total_tokens
+                  "total_tokens": response.usage.total_tokens,
+                  "timestamp": datetime.datetime.now().isoformat()
                   }
     except Exception as e:
         return {"status": "FAIL",
@@ -41,6 +46,27 @@ def call_llm(prompt):
                 "error": str(e)
                 }
 
+#load testcases from json file
+def load_testcases(file_path):
+    with open(file_path,"r",encoding="utf-8") as f:
+        testcases = json.load(f)
+    return testcases
+
+#run testcase
+def run_testcases(testcases):
+    results = []
+    for testcase in testcases:
+        print(f"Running {testcase['id']}.......")
+        result = call_llm(testcase["prompt"])
+        result["testcase_id"] = testcase.get("id","unknown")
+        results.append(result)
+    return results
+
+
+
 if __name__ == "__main__":
-    result = call_llm("请解释一下mbist是什么")
-    print(result)
+    testcases = load_testcases("./testcases/basic.json")
+    results = run_testcases(testcases)
+    for result in results:
+        print(f"report {result['testcase_id']} result")
+        print(result)
